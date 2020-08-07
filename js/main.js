a368ef038799083e62f12878ea5cfeeaedfb2b3f
Nikolay <68652897+SanistRay@users.noreply.github.com>
2020-08-07 12:59:45 +0300

    monile

--- js/main.js
+++ js/main.js
@@ -11,18 +11,18 @@ $(document).ready(function() {
 });
 $(document).ready(function() {
     $('#demo-button-id-1').click(function() {
-        $('.slider-img').attr('src', 'img/with-tooltip.png') //  -меняем атрибут src классу .класс
+        $('.slider-img').attr('src', 'img/with-tooltip.png')
     });
     $('#demo-button-id-2').click(function() {
-        $('.slider-img').attr('src', 'img/with-tooltip-1.png') //  -меняем атрибут src классу .класс
+        $('.slider-img').attr('src', 'img/with-tooltip-1.png')
     });
     $('#demo-button-id-3').click(function() {
-        $('.slider-img').attr('src', 'img/with-tooltip-4.png') //  -меняем атрибут src классу .класс
+        $('.slider-img').attr('src', 'img/with-tooltip-4.png')
     });
     $('#demo-button-id-4').click(function() {
-        $('.slider-img').attr('src', 'img/with-tooltip-2.png') //  -меняем атрибут src классу .класс
+        $('.slider-img').attr('src', 'img/with-tooltip-2.png')
     });
     $('#demo-button-id-5').click(function() {
-        $('.slider-img').attr('src', 'img/with-tooltip-3.png') //  -меняем атрибут src классу .класс
+        $('.slider-img').attr('src', 'img/with-tooltip-3.png')
     });
 });
